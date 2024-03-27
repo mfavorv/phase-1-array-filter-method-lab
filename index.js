1d7c9016035c7575b7489caadbda9f370954b303
Favor Vincent <vfavorm77@gmail.com>
2024-03-27 20:05:32 +0300

    JS Array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = [
+{name : "Job"},
+{name : "Jill"  },
+{name : "Jedida"},
+{name : "Cole"  },
+{name : "Holmes"},
+{name : "Doyle" }
+                ];
+
+const driverObjects = [
+{hometown: "Nairobi", name : "Job" },
+{hometown: "Kiambu" , name : "Jill"},
+{hometown: "Vihiga", name : "Jedida"},
+{hometown: "Bungoma", name : "Cole"},
+{hometown: "Kasarani", name : "Holmes"},
+{hometown: "Embu", name : "Doyle"}
+]                
+
+function findMatching(drivers, names){
+return  drivers.filter((driver) => driver.toLowerCase() === names.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letter){
+    return drivers.filter((names) => names.startsWith(letter))
+}
+
+function matchName(driverObjects, names){
+    return driverObjects.filter((itemMatch) => itemMatch.name === names)
+}
